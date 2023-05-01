4a7e2720a63f007dc37b2d3d6a147133366c708d
Shane <gupitshanehazel@gmail.com>
2023-05-01 13:03:14 +0800

    making of method

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,9 +1,12 @@
 # pseudocode
 
 # make a method
+def process ():
+
     # open a text file for writing
         # make a loop for the inputting of lines
             # ask user for a line
             # ask the user if there are more lines to add
             # if the user is done with their lines, break the loop
 # end of method
+process ()
