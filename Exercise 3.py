3bf6f5ce059ffcea4eef98fdfa6d4ffb40fbb25f
Shane <gupitshanehazel@gmail.com>
2023-05-01 13:00:40 +0800

    pseudocode

---
+++ Exercise 3.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# make a method
+    # open a text file for writing
+        # make a loop for the inputting of lines
+            # ask user for a line
+            # ask the user if there are more lines to add
+            # if the user is done with their lines, break the loop
+# end of method
