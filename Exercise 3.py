e5557b8466a91520f749af4facfde60c418593fe
Shane <gupitshanehazel@gmail.com>
2023-05-01 13:06:16 +0800

    make a loop for the inputting of user

--- Exercise 3.py
+++ Exercise 3.py
@@ -5,8 +5,10 @@ def process ():
 
     # open a text file for writing
     with open("mylife.txt", "w") as output_file:
-        
+
         # make a loop for the inputting of lines
+        while True:
+            
             # ask user for a line
             # ask the user if there are more lines to add
             # if the user is done with their lines, break the loop
